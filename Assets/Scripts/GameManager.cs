6b1a183cc592e835c6ff9b0cd11bc8b3ca371717
SmertNick <smertnick@gmail.com>
2021-10-16 18:26:33 +0300

    add basic setup in GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+  
+    [SerializeField] private PiecesSet piecesSet;
+    [SerializeField] private Settings settings;
+    [SerializeField] private PlayerNames playerNames;
+
+    private GameObject[,] pieces;
+
+    private List<Player> players;
+
+    void Awake()
     {
-        
+        if (instance != null && instance != this)
+            Destroy(gameObject);
+        else
+            instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        pieces = new GameObject[settings.BoardWidth, settings.BoardHeight];
+
+        SetUpPlayers(settings.AmountOfPlayers);
+    }
+
+    private void SetUpPlayers(int amountOfPlayers)
+    {
+        players = new List<Player>();
+        for (int i = 0; i < amountOfPlayers; i++)
+        {
+            players.Add(new Player(playerNames.GetPlayerName(i)));
+        }
     }
 }
